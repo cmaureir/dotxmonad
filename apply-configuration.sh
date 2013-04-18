5d6c585a77438ea7857bcc592d0bc713d423af36
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2013-04-18 17:46:36 -0300

    First configuration files of my xmonad
    Signed-off-by: Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>

---
+++ apply-configuration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.xmonad
+/bin/cp -f xmobarrc  ~/.xmonad/
+/bin/cp -f xmonad.hs ~/.xmonad/
+/bin/cp -f xinitrc   ~/.xinitrc
---
+++ xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/cmaureir/.xmonad/xmobarrc"
+  xmonad $ defaultConfig {
+    modMask = mod4Mask,
+    terminal = "urxvt",
+    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts $ layoutHook defaultConfig,
+    logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc,
+                          ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+  }`additionalKeys`
+      [(( mod4Mask, xK_F1), spawn "urxvt"),            -- to open firefox
+       (( mod4Mask, xK_F4), kill),                     -- to kill applications
+       (( controlMask, xK_KP_Add), sendMessage Expand) -- Ctrl + the plus (+) button to expand the master pane
+       ((0, xK_Print), spawn "scrot")                  -- use the print key to capture screenshot with scrot
+      ]
