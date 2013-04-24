921810533c685a1e0f59d194e617b18a4a9554a7
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2013-04-23 22:40:18 -0300

    importing x86
    Signed-off-by: Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>

--- xmonad.hs
+++ xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Gaps
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import Graphics.X11.ExtraTypes.XF86
 import System.IO
 
 main = do
@@ -24,7 +25,7 @@ main = do
       , (( mod4Mask, xK_F4), kill) -- to kill applications
       ,(( controlMask, xK_KP_Add), sendMessage Expand) -- Ctrl + the plus (+) button to expand the master pane
       , ((0, xK_Print), spawn "scrot") -- use the print key to capture screenshot with scrot
-      , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2-")
+      , ((0, xF86XK_AudioLowerVolume   ), pawn "amixer set Master 2-")
       , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
       , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
       ]
