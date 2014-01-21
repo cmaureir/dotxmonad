63a138e13d5eaf519dd11a074d9d791441b9f153
Cristian Maureira-Fredes <cmaurei@aei.mpg.de>
2014-01-21 14:34:23 +0100

    update configuration file
    Signed-off-by: Cristian Maureira-Fredes <cmaurei@aei.mpg.de>

--- xmonad.hs
+++ xmonad.hs
@@ -5,27 +5,36 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Gaps
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
 
+import XMonad.Layout.IM as IM
+import XMonad.Layout.Grid
+import Data.Ratio ((%))
+import XMonad.Layout.Spacing
+
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar /home/cmaureir/.xmonad/xmobarrc"
-  xmonad $ defaultConfig {
-    modMask = mod4Mask,
-    terminal = "urxvt",
-    manageHook = manageDocks <+> manageHook defaultConfig,
-    layoutHook = avoidStruts $ layoutHook defaultConfig,
-    logHook = dynamicLogWithPP $ xmobarPP
-                        { ppOutput = hPutStrLn xmproc,
-                          ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-  }`additionalKeys`
-      [(( mod4Mask, xK_F1), spawn "urxvt") -- to open firefox
-      , (( mod4Mask, xK_F4), kill) -- to kill applications
-      ,(( controlMask, xK_KP_Add), sendMessage Expand) -- Ctrl + the plus (+) button to expand the master pane
-      , ((0, xK_Print), spawn "scrot") -- use the print key to capture screenshot with scrot
-      , ((0, xF86XK_AudioLowerVolume   ), pawn "amixer set Master 2-")
-      , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
-      , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
-      ]
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobarrc"
+  xmonad $ defaultConfig
+    {
+        modMask = mod4Mask,
+        terminal = "urxvt",
+        manageHook = manageDocks <+> manageHook defaultConfig,
+        layoutHook = avoidStruts $ layoutHook defaultConfig,
+        logHook = dynamicLogWithPP $ xmobarPP
+                            { ppOutput = hPutStrLn xmproc,
+                              ppTitle = xmobarColor "green" "" . shorten 50
+                            }
+    } `additionalKeysP` myKeysP
+
+myKeysP = [ ("<XF86MonBrightnessUp>",   spawn "~/bin/brightness +")
+          , ("<XF86MonBrightnessDown>", spawn "~/bin/brightness -")
+          , ("<XF86AudioLowerVolume>",  spawn "amixer set Master 2-")
+          , ("<XF86AudioRaiseVolume>",  spawn "amixer set Master 2+")
+          , ("<XF86AudioMute>",         spawn "amixer set Master toggle")
+          , ("M-<F1>",                  spawn "urxvt")
+          , ("M-<F2>",                  spawn "gmrun")
+          , ("M-<F4>",                  kill)
+          , ("<Print>",                 spawn "scrot")
+          ]
