909151026a654c1a1355432a53405e605f871c10
Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
2018-02-02 16:47:43 +0100

    new version with spacing
    Signed-off-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Spacing
 
 myKeysP = [ ("<XF86MonBrightnessUp>",   spawn "~/bin/brightness +")
           , ("<XF86MonBrightnessDown>", spawn "~/bin/brightness -")
@@ -14,6 +15,9 @@ myKeysP = [ ("<XF86MonBrightnessUp>",   spawn "~/bin/brightness +")
           , ("M-<F2>",                  spawn "gmrun")
           , ("M-<F3>",                  spawn "xterm")
           , ("M-<F4>",                  kill)
+          , ("M-<F7>",         spawn "~/bin/volume -")
+          , ("M-<F8>",         spawn "~/bin/volume +")
+          , ("M-<F9>",         spawn "~/bin/volume m")
           , ("<Print>",                 spawn "scrot")
           ]
 
@@ -21,7 +25,7 @@ myKeysP = [ ("<XF86MonBrightnessUp>",   spawn "~/bin/brightness +")
 myBar = "xmobar ~/.xmonad/xmobarrc"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "[" "]" }
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
@@ -29,10 +33,11 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 -- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig
         { manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook = smartBorders (avoidStruts  $  layoutHook defaultConfig)
-        , borderWidth = 1
+        , layoutHook = smartSpacing 5 $ smartBorders (avoidStruts  $  layoutHook defaultConfig)
+        , borderWidth = 0
         , terminal = "terminator"
         , focusedBorderColor = "#ff4747"
+        , focusFollowsMouse = False
         , normalBorderColor = "#DDDDDD"
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeysP` myKeysP
