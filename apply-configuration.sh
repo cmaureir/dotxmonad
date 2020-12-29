ff289669a3485d36e711779c7bcb20ffbec8cbdf
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-12-29 01:38:31 +0100

    Update xprofile and apply-configuration script

--- apply-configuration.sh
+++ apply-configuration.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/.xmonad
-/bin/cp -f xmobarrc  ~/.xmonad/
-/bin/cp -f xmonad.hs ~/.xmonad/
-/bin/cp -f xinitrc   ~/.xinitrc
-/bin/cp -f xprofile  ~/.xprofile
+mkdir -p ~/.xmonad || echo "Error creating ~/.xmonad directory"
+/bin/cp -f xmobarrc  ~/.xmonad/ || echo "Error copying 'xmobarrc'"
+/bin/cp -f xmonad.hs ~/.xmonad/ || echo "Error copying 'xmonad.hs'"
+/bin/cp -f .xprofile  ~/.xprofile || echo "Error copying '.xprofile'"
