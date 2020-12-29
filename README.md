c7515ec9de4bd40eddbc9d03d11801346ba60eb1
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-12-29 01:34:39 +0100

    Update 2020 Workstation

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-dotxmonad
-=========
+# dotxmonad
 
-Xmonad related configuration files
+Simple xmonad configuration files, using smart spacing, a floating workspace,
+and custom scripts to handle volume, brightness, and other simple
+functionalities that you can find on my [bin
+repository](https://github.com/cmaureir/bin).
 
-Features
----------
+I am currently using [xmobar](https://xmobar.org) with a simple configuration,
+using only a fallback Emoji font to add icons in the categories, and leaving
+space for the trayer, [stalonetray](http://stalonetray.sourceforge.net/), to
+use the upper right space.
 
- * xmonad
-  * ...
-  * ...
- * xmobar
-  * ...
-  * ...
+## Screenshot
 
-Screenshots
-------------
+Two screens configuration:
 
-![ScreenShot](https://raw.github.com/cmaureir/dotxmonad/master/screenshots/2013-04-18-191605_1024x600_scrot.png)
+![ScreenShot](screenshots/two-screens.png)
--- xmonad.hs
+++ xmonad.hs
@@ -1,47 +1,90 @@
 import XMonad
+import qualified Codec.Binary.UTF8.String as UTF8
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig(additionalKeysP)
-import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Layout.Spacing
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Tabbed
+import XMonad.StackSet (RationalRect (..), currentTag)
 
-myKeysP = [ ("<XF86MonBrightnessUp>",   spawn "~/bin/brightness +")
-          , ("<XF86MonBrightnessDown>", spawn "~/bin/brightness -")
-          , ("<XF86AudioLowerVolume>",  spawn "~/bin/volume -")
-          , ("<XF86AudioRaiseVolume>",  spawn "~/bin/volume +")
-          , ("<XF86AudioMute>",         spawn "~/bin/volume m")
-          , ("M-<F1>",                  spawn "terminator")
-          , ("M-<F2>",                  spawn "gmrun")
-          , ("M-<F3>",                  spawn "xterm")
-          , ("M-<F4>",                  kill)
-          , ("M-<F7>",         spawn "~/bin/volume -")
-          , ("M-<F8>",         spawn "~/bin/volume +")
-          , ("M-<F9>",         spawn "~/bin/volume m")
-          , ("<Print>",                 spawn "scrot")
-          ]
+myKeysP = [
+    , ("<XF86AudioLowerVolume>",  spawn "~/bin/volume -")
+    , ("<XF86AudioRaiseVolume>",  spawn "~/bin/volume +")
+    , ("<XF86AudioMute>",         spawn "~/bin/volume m")
+    , ("<XF86AudioNext>",         spawn "~/bin/player next")
+    , ("<XF86AudioPrev>",         spawn "~/bin/player prev")
+    , ("<XF86AudioPlay>",         spawn "~/bin/player play")
+    , ("<XF86AudioPause>",        spawn "~/bin/player pause")
+    , ("<XF86AudioStop>",         spawn "~/bin/player stop")
+    , ("M-<F1>",                  spawn "konsole")
+    , ("M-<Esc>",                 spawn "konsole")
+    , ("M-<F2>",                  spawn "~/bin/run")
+    , ("M-<F3>",                  spawn "kitty")
+    , ("M-<F4>",                  kill)
+    , ("M-<F7>",                  spawn "~/bin/volume -")
+    , ("M-<F8>",                  spawn "~/bin/volume +")
+    , ("M-<F9>",                  spawn "~/bin/volume m")
+    , ("<Print>",                 spawn "scrot")
+    ]
+
+myWorkspaces = [
+      "1"
+    , "2"
+    , "3"
+    , "4"
+    , "5"
+    , "6"
+    , "7"
+    , "8"
+    , "9"
+    ]
 
 -- Command to launch the bar.
 myBar = "xmobar ~/.xmonad/xmobarrc"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "[" "]" }
+myPP = xmobarPP {
+    ppTitle = (\str -> "")
+    , ppCurrent = xmobarColor "#778eca" "" . wrap "[" "]"
+    , ppOrder = \(ws:l:t:_)   -> [ws]
+}
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
+tabConfig = defaultTheme {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
+
 -- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig
-        { manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook = smartSpacing 5 $ smartBorders (avoidStruts  $  layoutHook defaultConfig)
-        , borderWidth = 0
-        , terminal = "terminator"
-        , focusedBorderColor = "#ff4747"
-        , focusFollowsMouse = False
-        , normalBorderColor = "#DDDDDD"
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        } `additionalKeysP` myKeysP
+    { manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig <+> (onFloatingWorkSpace --> doFloat)
+    , workspaces = myWorkspaces
+    , layoutHook = smartSpacing 5 $ onWorkspace "8" simplestFloat $ smartBorders (avoidStruts  $  layoutHook defaultConfig)
+    , borderWidth = 0
+    , terminal = "terminator"
+    , focusedBorderColor = "#ff4747"
+    , focusFollowsMouse = False
+    , normalBorderColor = "#DDDDDD"
+    , modMask = mod4Mask
+} `additionalKeysP` myKeysP
+
+onFloatingWorkSpace :: Query Bool
+onFloatingWorkSpace = liftX $
+    withWindowSet (return . (`elem` floating) . currentTag)
+  where
+    floating = ["8"]
 
 -- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-
+main :: IO ()
+main = do
+    xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
